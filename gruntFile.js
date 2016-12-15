44902d890f568c75e2b6e5872ec910c0a4d3504f
Mike Wilcox <mwilcox@paycor.com>
2016-12-15 17:16:41 -0600

    add less, heya-io

--- gruntFile.js
+++ gruntFile.js
@@ -3,51 +3,19 @@ var path = require('path');
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        //'http-server': {
-        //    'dev': {
-        //
-        //        // the server root directory
-        //        root: '.',
-        //
-        //        // the server port
-        //        // can also be written as a function, e.g.
-        //        // port: function() { return 8282; }
-        //        port: 8282,
-        //
-        //        // the host ip address
-        //        // If specified to, for example, "127.0.0.1" the server will
-        //        // only be available on that ip.
-        //        // Specify "0.0.0.0" to be available everywhere
-        //        //host: "0.0.0.0",
-        //
-        //        //cache: <sec>,
-        //        showDir : true,
-        //        autoIndex: true,
-        //
-        //        // server default file extension
-        //        ext: "html",
-        //
-        //        // run in parallel with other tasks
-        //        runInBackground: 0,
-        //
-        //        // specify a logger function. By default the requests are
-        //        // sent to stdout.
-        //        //logFn: function(req, res, error) { },
-        //
-        //        // Proxies all requests which can't be resolved locally to the given url
-        //        // Note this this will disable 'showDir'
-        //        //proxy: "http://someurl.com",
-        //
-        //        // Tell grunt task to open the browser
-        //        openBrowser : false,
-        //
-        //        // customize url to serve specific pages
-        //        //customPages: {
-        //        //    "/readme": "README.md",
-        //        //    "/readme.html": "README.html"
-        //        //
-        //    }
-        //}
+        metl:{
+            less:{
+                main:{
+                    src:'less/main.less',
+                    output: 'dist/main.css'
+                }
+            },
+            watch:{
+                less:['./less/*.less'],
+                scripts:['./lib/**/*.js', './ui/**/*.html'],
+                port: 35731
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-metl-tools');
